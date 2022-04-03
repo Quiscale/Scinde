c409624067d03d8f527a7125967562c037b42d98
Quiscale dev <quiscale.dev@gmail.com>
2022-04-03 09:19:56 +0200

    Change Keyboard behavior

--- src/scinde/controller/KeyboardController.java
+++ src/scinde/controller/KeyboardController.java
@@ -12,10 +12,6 @@ public class KeyboardController implements EventHandler<KeyEvent> {
 	// ////////////////////////////////////////////////////////////////////////
 
 	private static float velocity = 10;
-	private boolean upPressed = false;
-	private boolean downPressed = false;
-	private boolean leftPressed = false;
-	private boolean rightPressed = false;
 	
 	// ////////////////////////////////////////////////////////////////////////
 	// Constructors
@@ -36,40 +32,17 @@ public class KeyboardController implements EventHandler<KeyEvent> {
 		
 		if(event.getEventType() == KeyEvent.KEY_PRESSED) {
 			
-			if(event.getCode() == KeyCode.UP && !upPressed) {
-				upPressed = true;
-				velocityVec.setX(velocityVec.getX() + velocity);
-			}
-			if(event.getCode() == KeyCode.DOWN && !downPressed) {
-				downPressed = true;
-				velocityVec.setX(velocityVec.getX() - velocity);
-			}
-			if(event.getCode() == KeyCode.LEFT && !leftPressed) {
-				leftPressed = true;
-				velocityVec.setY(velocityVec.getY() + velocity);
-			}
-			if(event.getCode() == KeyCode.RIGHT && !rightPressed) {
-				rightPressed = true;
-				velocityVec.setY(velocityVec.getY() - velocity);
-			}
-		}
-		if(event.getEventType() == KeyEvent.KEY_RELEASED) {
-			
 			if(event.getCode() == KeyCode.UP) {
-				upPressed = false;
-				velocityVec.setX(velocityVec.getX() - velocity);
+				velocityVec.setY(-velocity);
 			}
 			if(event.getCode() == KeyCode.DOWN) {
-				downPressed = false;
-				velocityVec.setX(velocityVec.getX() + velocity);
+				velocityVec.setY(+velocity);
 			}
 			if(event.getCode() == KeyCode.LEFT) {
-				leftPressed = false;
-				velocityVec.setY(velocityVec.getY() - velocity);
+				velocityVec.setX(-velocity);
 			}
 			if(event.getCode() == KeyCode.RIGHT) {
-				rightPressed = false;
-				velocityVec.setY(velocityVec.getY() + velocity);
+				velocityVec.setX(+velocity);
 			}
 		}
 		
